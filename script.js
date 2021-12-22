b02b1b0b5480aaae804a87c032d2effe9fb621a4
Gaurav Upreti <gaurav.urt@gmail.com>
2021-12-22 12:46:44 +0530

    game functionality added

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let highScore = 0;
+document.querySelector('.highscore').textContent = highScore;
+
+let score = 20;
+document.querySelector('.score').textContent = score;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  //   console.log('guess = ' + guess + ', type = ' + typeof guess);
+  console.log(
+    'secretNumber = ' + secretNumber + ', type = ' + typeof secretNumber
+  );
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉️ Correct Number';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈️ too high';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉️ too low';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+});
